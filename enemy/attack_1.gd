7c3586daeb535251e7889f2e039f48ca75ebf868
Eugene <eugene.kotyashov@gmail.com>
2026-04-03 19:00:03 +0300

    added animation player to time attack area activation, added tilemap platforms

--- enemy/attack_1.gd
+++ enemy/attack_1.gd
@@ -1,17 +1,14 @@
 extends State
 
-@export var attack1_area : Area2D
+@export var property_player: AnimationPlayer
 
-func set_atack_area_active(active : bool):
-	attack1_area.set_deferred( "monitorable", active)
-	attack1_area.set_deferred( "monitoring", active)
 	
 func _ready() -> void:
 	name = "attack1_state"
 	
 func on_enter() -> void:
 	super.on_enter()
-	set_atack_area_active(true)
+	property_player.play("attack1_area_active")
 	
 func on_exit() -> void:
-	set_atack_area_active(false)
+	property_player.stop()
--- player/attack_1.gd
+++ player/attack_1.gd
@@ -1,21 +1,14 @@
 extends State
 
-@export var attack_area : Area2D
+@export var property_player: AnimationPlayer
 
+	
 func _ready() -> void:
 	name = "attack1_state"
-
-func set_atack_area_active(active : bool):
-	attack_area.set_deferred( "monitorable", active)
-	attack_area.set_deferred( "monitoring", active)
 	
 func on_enter() -> void:
 	super.on_enter()
-	set_atack_area_active(true)
-
-func on_exit() -> void:
-	set_atack_area_active(false)
+	property_player.play("attack1_area_active")
 	
-func update(_delta: float) -> void:
-	pass
-	#print("attack1")
+func on_exit() -> void:
+	property_player.stop()
